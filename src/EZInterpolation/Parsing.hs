ede04eb3be02032c1d5a0a0cdc6d28b65095a4eb
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-03-14 22:48:59 +0400

    Parse lines

--- src/EZInterpolation/Parsing.hs
+++ src/EZInterpolation/Parsing.hs
@@ -1,18 +1,35 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
 module EZInterpolation.Parsing where
 
 import Prelude ()
-import ClassyPrelude hiding (try)
-import Text.Parsec
+import ClassyPrelude hiding (try, lines)
+import Text.Parsec hiding (Line)
 
-data Part =
-  PartText [Char] |
-  PartIdentifier [Char]
+data Line = 
+  Line {lineIndent :: Int, lineContents :: [LineContent]}
+  deriving (Show)
 
-parseParts :: [Char] -> Either ParseError [Part]
-parseParts = parse parts "EZInterpolation"
+data LineContent = 
+  LineContentText [Char] |
+  LineContentIdentifier [Char]
+  deriving (Show)
+
+
+parseLines :: [Char] -> Either ParseError [Line]
+parseLines = parse lines "EZInterpolation.Parsing.parseLines"
   where
-    parts = manyTill (identifier <|> text) eof
-    identifier = fmap PartIdentifier $ string "$" *> many1 alphaNum
-    text = fmap PartText $ manyTill anyChar $ 
-      (try $ void $ lookAhead identifier) <|> eof
-
+    lines = sepBy line newline <* eof
+    line = Line <$> countIndent <*> many content
+    countIndent = fmap length $ try $ lookAhead $ many $ char ' '
+    content = try identifier <|> contentText
+    identifier = fmap LineContentIdentifier $ string "$" *> many1 alphaNum
+    contentText = do
+      text <- manyTill anyChar end
+      if null text
+        then fail "Empty text"
+        else return $ LineContentText $ text
+      where
+        end = 
+          (void $ try $ lookAhead identifier) <|> 
+          (void $ try $ lookAhead newline) <|> 
+          eof
