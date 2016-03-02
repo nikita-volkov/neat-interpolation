a4a14cde1b07b63186f90a2ba220c731dbfb8a5c
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-03-02 08:24:39 +0300

    Update the tests

--- executables/APITests.hs
+++ executables/APITests.hs
@@ -10,6 +10,26 @@ main = htfMain $ htf_thisModulesTests
 
 test_demo = do
   assertEqual
+    "function(){\n  function(){\n    {\n      indented line\n      indented line\n    }\n  }\n  return {\n    indented line\n    indented line\n  }\n}\n"
+    (template a a)
+  assertEqual
+    "this_could_be_one_long_identifier\n"
+    (escaped "one")
+  where
+    template a b = 
+      [text|
+        function(){
+          function(){
+            $a
+          }
+          return $b
+        }
+      |]
+    escaped name = [text|this_could_be_${name}_long_identifier|]
+    a = "{\n  indented line\n  indented line\n}"
+
+test_dollar = do
+  assertEqual
     "function(){\n  function(){\n    {\n      indented line\n      indented line\n    }\n  }\n  return \"$b\"\n}\n"
     (template a a)
   assertEqual
@@ -27,4 +47,3 @@ test_demo = do
       |]
     escaped name = [text|this_could_be_$$${name}$$_long_identifier|]
     a = "{\n  indented line\n  indented line\n}"
-
